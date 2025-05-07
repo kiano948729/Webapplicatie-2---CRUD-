f8fbc005dc81e4a043059ee30ff565bf9eb3ad28
kiano948729 <148862757+kiano948729@users.noreply.github.com>
2025-05-07 12:33:13 +0200

    vakantiespagina hardcoded toegevoegd- voorbereiding met search en data voor verbinding db

---
+++ components/vakanties-blok.php
@@ -0,0 +1,43 @@
+<div class="vakanties-main-blok">
+    <h2>Find the best place</h2>
+
+    <div class="input-rij">
+        <div class="invoer-blok">
+            <label for="locatie">Location</label>
+            <input type="text" id="locatie" name="locatie" placeholder="Type the destination">
+        </div>
+
+        <div class="invoer-blok">
+            <label for="check-in">Check in</label>
+            <input type="date" id="check-in" name="check-in">
+        </div>
+
+        <div class="invoer-blok">
+            <label for="check-out">Check out</label>
+            <input type="date" id="check-out" name="check-out">
+        </div>
+
+        <div class="invoer-blok">
+            <label for="deelnemers">Participants</label>
+            <input type="number" id="deelnemers" name="deelnemers" placeholder="Add guests">
+        </div>
+    </div>
+
+    <div class="filters">
+        <input type="radio" id="filter-huis" name="filter" value="house">
+        <label for="filter-huis">House</label>
+
+        <input type="radio" id="filter-hotel" name="filter" value="hotel">
+        <label for="filter-hotel">Hotel</label>
+
+        <input type="radio" id="filter-residentieel" name="filter" value="residential">
+        <label for="filter-residentieel">Residential</label>
+
+        <input type="radio" id="filter-appartement" name="filter" value="apartment">
+        <label for="filter-appartement">Apartment</label>
+    </div>
+
+    <div class="zoek-knop">
+        <button type="submit">Search Properties</button>
+    </div>
+</div>
