4ec2db76e5481f14cdde9fc04b554740b65051d2
Joe74012 <1210764@student.roc-nijmegen.nl>
2025-06-10 12:50:30 +0200

    nieuwe knop toegevoegd aan admin paneel

--- backend/admin/admin.php
+++ backend/admin/admin.php
@@ -36,6 +36,7 @@ if (!isset($conn)) {
             <button class="tab-button" data-tab="boeking_deal">Boekingen deals</button>
             <button class="tab-button" data-tab="boeking_accomodatie">Boekingen accommodaties</button>
             <button class="tab-button" data-tab="addomodatie_review">review accommodaties</button>
+            <button class="tab-button" data-tab="Contact_bericht">Contact berichten</button>
         </div>
 
         <div class="search-bar">
@@ -65,6 +66,10 @@ if (!isset($conn)) {
 
             <section id="addomodatie_review" class="tab-content">
                 <?php include 'reviews/list.php'; ?>
+            </section>
+
+            <section id="Contact_bericht-section" class="tab-content">
+                <?php include 'contact/list.php'; ?>
             </section>
         </div>
     </main>
