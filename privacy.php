cc4eafe11c0a7baf2c572076f2178381d7e5aeea
Joe74012 <1210764@student.roc-nijmegen.nl>
2025-06-11 12:11:40 +0200

    Nieuwe layout van Privacy page

--- privacy.php
+++ privacy.php
@@ -25,7 +25,7 @@ if (isset($_SESSION['user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="google" content="notranslate">
-    <title>Privacy | Naam</title>
+    <title>Over ons | Backpack & go</title>
     <link rel="stylesheet" href="css/JoeStyle.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="icon" href="../img/CompassLogo.png" type="Images/png">
@@ -40,31 +40,25 @@ if (isset($_SESSION['user_id'])) {
     <?php require_once("components/header.php") ?>
 </header>
 <main class="privacyMain">
-    <div class="achtergrondPrivacy">
-        <h1 class="Witte-Text">Privacybeleid</h1>
+    <div class="achtergrondOverOns">
+        <h1 class="Witte-Text">Over ons</h1>
     </div>
-    <div class="titelCenter">
-        <h1>De informatie de wij verzamelen</h1>
-        <p class="normaalText">Wanneer jij de backpack & go site gebruikt gebruiken wij verschillende informatie voor verschillende redenenen de informatie die we gebruiken en wat wij doen met die informatie kan je hieronder zien.</p>
+    <div class="centerTextDiv">
+        <p>Als een backpack vakantiebureau platform specialiseren wij in het maken van een platform die makkelijk is om te gebruiken waar je backpack vakanties kan boeken</p>
     </div>
-    <div class="titelCenter">
-        <h1>Account gegevens</h1>
-        <p class="normaalText">Wij gebruiken account gegevens zoals:</p>
-        <ol>
-            <li>Emails</li>
-            <li>Wachtwoorden</li>
-            <li>Gebruikersnamen</li>
-            <li>Achternamen</li>
-            <li>Geboortedatums</li>
-        </ol>
+    <div class="splitDivRij">
+        <img class="afbeeldingDivLinks" src="img/imageMain.png">
+        <div class="textColumnDiv">
+            <h1 class="boldText">Wat is backpack & go?</h1>
+            <p>Backpack & go is een backpacking vakantie service waar als gebruiker u een backpack vakantie reis kan boeken via onze service.</p>
+        </div>
     </div>
-    <div class="titelCenter">
-        <h1>Hoe wij de informatie gebruiken</h1>
-        <ol>
-            <li>Account gegevens vergelijken: Om de gebruikers informatie zoals wachtwoorden en namen te vergelijken om u in te kunnen loggen.</li>
-            <li>Communicatie: Om te communiceren met u via u email als u vragen heeft over onze service.</li>
-            <li>Service verbeteren: Als u bijvoorbeeld reviews achterlaat of andere feedback dan kunnen wij die feedback gebruiken om onze service te verbeteren.</li>
-        </ol>
+    <div class="splitDivRijTwee">
+        <div class="textColumnDiv">
+            <h1 class="boldText">Waarom Backpack & go?</h1>
+            <p class="centerText">Backpack & go is een service die constant word verbeterd en dat komt voornamelijk door feedback die wij ontvangen van onze gebruikers.<a class="blauwText" href="Contact.php">als u feedback wilt geven over onze service klik dan hier</a> </p>
+        </div>
+        <img class="afbeeldingDivLinks" src="img/imageBerg.png">
     </div>
 </main>
 </body>
